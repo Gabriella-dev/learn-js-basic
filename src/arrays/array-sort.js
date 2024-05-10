c1ccd7763892f26bb7b16d3b4d85256e7ba24781
Gabriela Tiplea <gabriella.tiplea@cinch.co.uk>
2024-05-10 16:01:55 +0100

    feat: add array-sort and tests

---
+++ src/arrays/array-sort.js
@@ -0,0 +1,148 @@
+// 1. create a function that takes an array of string and return sorted in alphabetical order
+// the input is an array of strings
+// the function should return an array of strings sorted in alphabetical order
+// if the input is an empty array, return []
+// the input array will have only strings
+// examples:
+// alphabeticalOrder(["dog", "wolf", "by", "family", "eaten", "camping"]); // ["by", "camping", "dog", "eaten", "family", "wolf"]
+// alphabeticalOrder(["dog", "wolf", "by", "family", "eaten", "camping", ""]); // ["", "by", "camping", "dog", "eaten", "family", "wolf"]
+// alphabeticalOrder([]); // []
+// */
+export const alphabeticalOrder = (stringsArray) => {};
+
+// 2. create a function that takes an array of string and return sorted in reverse alphabetical order
+// the input is an array of strings
+// the function should return an array of strings sorted in reverse alphabetical order
+// if the input is an empty array, return []
+// the input array will have only strings
+/*examples:
+reverseAlphabeticalOrder(["dog", "wolf", "by", "family", "eaten", "camping"]); // ["wolf", "family", "eaten", "dog", "camping", "by"]
+reverseAlphabeticalOrder(["dog", "wolf", "by", "family", "eaten", "camping", ""]); // ["wolf", "family", "eaten", "dog", "camping", "by", ""]
+reverseAlphabeticalOrder([]); // []
+*/
+
+export const reverseAlphabeticalOrder = (stringsArray) => {};
+
+// 3. create a function that takes an array of numbers and return sorted in ascending order
+// the input is an array of numbers (integers)
+// the function should return an array of numbers sorted in ascending order
+// if the input is an empty array, return []
+// the input array will have only numbers
+/*examples:
+ascendingOrder([4, 3, 2, 1]); // [1, 2, 3, 4]
+ascendingOrder([1, 2, 3, 4]); // [1, 2, 3, 4]
+ascendingOrder([]); // []
+*/
+
+export const ascendingOrder = (numbersArray) => {};
+// 4. create a function that takes an array of numbers and return sorted in descending order
+// the input is an array of numbers (integers)
+// the function should return an array of numbers sorted in descending order
+// the function should return an array of numbers sorted in descending order
+// if the input is an empty array, return []
+// the input array will have only numbers
+/*examples:
+descendingOrder([4, 3, 2, 1]); // [4, 3, 2, 1]
+descendingOrder([1, 2, 3, 4]); // [4, 3, 2, 1]
+descendingOrder([]); // []
+*/
+
+export const descendingOrder = (numbersArray) => {};
+
+// 5. create a function that takes an array of objects and return sorted by a key in ascending order
+// the input is an array of objects
+// the function should return an array of objects sorted by a key in ascending order
+// the key will always be present in the object
+// the input array will have only objects
+// the key will be a string
+// the value of the key will be a number
+/*examples:
+ascendingOrderByKey(
+  [
+    { name: "John", age: 23 },
+    { name: "Jane", age: 24 },
+    { name: "Doe", age: 21 },
+  ],
+  "age"
+);
+// [
+//   { name: "Doe", age: 21 },
+//   { name: "John", age: 23 },
+//   { name: "Jane", age: 24 },
+// ]
+]    
+ascendingOrderByKey([], "age"); // []
+*/
+
+export const ascendingOrderByKey = (objectsArray, key) => {
+  return objectsArray.sort((a, b) => {
+    return a[key] - b[key];
+  });
+};
+
+// 6. create a function that takes an array of objects and return sorted by a key in descending order
+// the input is an array of objects
+// the function should return an array of objects sorted by a key in descending order
+// the key will always be present in the object
+// the input array will have only objects
+// the key will be a string
+// the value of the key will be a number
+/*examples:
+descendingOrderByKey(
+  [
+    { name: "John", age: 23 },
+    { name: "Jane", age: 24 },
+    { name: "Doe", age: 21 },
+  ],
+  "age"
+);  
+[
+ { name: "Jane", age: 24 },
+{ name: "John", age: 23 },
+{ name: "Doe", age: 21 },
+]
+descendingOrderByKey([], "age"); // []
+*/
+
+export const descendingOrderByKey = (objectsArray, key) => {};
+// 7. create a function that takes an array of objects and return sorted by a key in ascending order
+// if the key is not present in one of the objects, it should be at the end
+// the input is an array of objects
+// the function should return an array of objects sorted by a key in ascending order
+// if  the key is not present in one of the objects, it should be at the end
+// the input array will have only objects
+// the key will be a string
+// the value of the key will be a number
+/*examples:
+ascendingOrderByKey(
+  [
+    { name: "John", age: 23 },
+    { name: "Jane", age: 24 },
+    { name: "Doe", age: 21 },
+  ],
+  "age"
+);      
+[
+ { name: "Doe", age: 21 },
+ { name: "John", age: 23 },
+ { name: "Jane", age: 24 },
+]
+
+ascendingOrderByKey(
+  [
+    { name: "John", age: 23 },
+    { name: "Doe" },
+    { name: "Jane", age: 24 },
+   
+  ],
+  "age"
+);  [
+    { name: "John", age: 23 },
+    { name: "Jane", age: 24 },
+     { name: "Doe" },
+]
+
+ascendingOrderByKey([], "age"); // []
+*/
+
+export const ascendingOrderByKeyWithMissingKey = {};
---
+++ src/arrays/array-sort.test.js
@@ -0,0 +1,171 @@
+import {
+  alphabeticalOrder,
+  reverseAlphabeticalOrder,
+  ascendingOrder,
+  descendingOrder,
+  ascendingOrderByKey,
+  descendingOrderByKey,
+  ascendingOrderByKeyWithMissingKey,
+} from "./array-sort";
+
+describe.skip("alphabeticalOrder", () => {
+  test("should return an empty array if the input is an empty array", () => {
+    expect(alphabeticalOrder([])).toEqual([]);
+  });
+
+  test("should return an array of strings sorted in alphabetical order", () => {
+    expect(
+      alphabeticalOrder(["dog", "wolf", "by", "family", "eaten", "camping"])
+    ).toEqual(["by", "camping", "dog", "eaten", "family", "wolf"]);
+    expect(
+      alphabeticalOrder(["dog", "wolf", "by", "family", "eaten", "camping", ""])
+    ).toEqual(["", "by", "camping", "dog", "eaten", "family", "wolf"]);
+  });
+});
+
+describe.skip("reverseAlphabeticalOrder", () => {
+  test("should return an empty array if the input is an empty array", () => {
+    expect(reverseAlphabeticalOrder([])).toEqual([]);
+  });
+
+  test("should return an array of strings sorted in reverse alphabetical order", () => {
+    expect(
+      reverseAlphabeticalOrder([
+        "dog",
+        "wolf",
+        "by",
+        "family",
+        "eaten",
+        "camping",
+      ])
+    ).toEqual(["wolf", "family", "eaten", "dog", "camping", "by"]);
+    expect(
+      reverseAlphabeticalOrder([
+        "dog",
+        "wolf",
+        "by",
+        "family",
+        "eaten",
+        "camping",
+        "",
+      ])
+    ).toEqual(["wolf", "family", "eaten", "dog", "camping", "by", ""]);
+  });
+});
+
+describe.skip("ascendingOrder", () => {
+  test("should return an empty array if the input is an empty array", () => {
+    expect(ascendingOrder([])).toEqual([]);
+  });
+
+  test("should return an array of numbers sorted in ascending order", () => {
+    expect(ascendingOrder([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
+    expect(ascendingOrder([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+    expect(ascendingOrder([1, 11, 3, 4, 44, 33, 2, 22])).toEqual([
+      1, 2, 3, 4, 11, 22, 33, 44,
+    ]);
+  });
+});
+
+describe.skip("descendingOrder", () => {
+  test("should return an empty array if the input is an empty array", () => {
+    expect(descendingOrder([])).toEqual([]);
+  });
+
+  test("should return an array of numbers sorted in descending order", () => {
+    expect(descendingOrder([4, 3, 2, 1])).toEqual([4, 3, 2, 1]);
+    expect(descendingOrder([1, 2, 3, 4])).toEqual([4, 3, 2, 1]);
+    expect(descendingOrder([1, 11, 3, 4, 44, 33, 2, 22])).toEqual([
+      44, 33, 22, 11, 4, 3, 2, 1,
+    ]);
+  });
+});
+
+describe.skip("ascendingOrderByKey", () => {
+  test("should return an empty array if the input is an empty array", () => {
+    expect(ascendingOrderByKey([], "age")).toEqual([]);
+  });
+
+  test("should return an array of objects sorted by a key in ascending order", () => {
+    expect(
+      ascendingOrderByKey(
+        [
+          { name: "dog", age: 3 },
+          { name: "wolf", age: 4 },
+          { name: "by", age: 1 },
+          { name: "family", age: 2 },
+          { name: "eaten", age: 11 },
+          { name: "camping", age: 6 },
+        ],
+        "age"
+      )
+    ).toEqual([
+      { name: "by", age: 1 },
+      { name: "family", age: 2 },
+      { name: "dog", age: 3 },
+      { name: "wolf", age: 4 },
+      { name: "camping", age: 6 },
+      { name: "eaten", age: 11 },
+    ]);
+  });
+});
+
+describe.skip("descendingOrderByKey", () => {
+  test("should return an empty array if the input is an empty array", () => {
+    expect(descendingOrderByKey([], "age")).toEqual([]);
+  });
+
+  test("should return an array of objects sorted by a key in descending order", () => {
+    expect(
+      descendingOrderByKey(
+        [
+          { name: "dog", age: 3 },
+          { name: "wolf", age: 4 },
+          { name: "by", age: 1 },
+          { name: "family", age: 2 },
+          { name: "eaten", age: 11 },
+          { name: "camping", age: 6 },
+        ],
+        "age"
+      )
+    ).toEqual([
+      { name: "eaten", age: 11 },
+      { name: "camping", age: 6 },
+      { name: "wolf", age: 4 },
+      { name: "dog", age: 3 },
+      { name: "family", age: 2 },
+      { name: "by", age: 1 },
+    ]);
+  });
+});
+
+describe.skip("ascendingOrderByKeyWithMissingKey", () => {
+  test("should return an empty array if the input is an empty array", () => {
+    expect(ascendingOrderByKeyWithMissingKey([], "age")).toEqual([]);
+  });
+
+  test("should return an array of objects sorted by a key in ascending order", () => {
+    expect(
+      ascendingOrderByKeyWithMissingKey(
+        [
+          { name: "dog", age: 3 },
+          { name: "wolf", age: 4 },
+          { name: "by", age: 1 },
+          { name: "family", age: 2 },
+          { name: "eaten", age: 11 },
+          { name: "camping", age: 6 },
+          { name: "missing" },
+        ],
+        "age"
+      )
+    ).toEqual([
+      { name: "by", age: 1 },
+      { name: "family", age: 2 },
+      { name: "dog", age: 3 },
+      { name: "wolf", age: 4 },
+      { name: "camping", age: 6 },
+      { name: "eaten", age: 11 },
+      { name: "missing" },
+    ]);
+  });
+});
